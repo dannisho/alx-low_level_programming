b5f6e1ed58c94ab16f2e61ba66f4cfe5c6362501
dannisho <danielgelandu@gmail.com>
2022-10-14 01:39:01 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - main entery function
+ * Return: 0
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+
+	return (1);
+}
