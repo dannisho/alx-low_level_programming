ac7e52a0afd25c73ed78928fea514d7a22382ace
dannisho <danielgelandu@gmail.com>
2022-10-14 00:48:54 -0700

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - main entery function
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	printf("\with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
