88f129a557bc414ec6c6326068375bb2c706ae2c
dannisho <danielgelandu@gmail.com>
2022-10-14 00:44:37 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main enetry function
+ *
+ * Return: 0 (success)
+ */
+
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
