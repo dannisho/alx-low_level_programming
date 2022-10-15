bea16701cd43a6cad079dcfe4b68a40571e9807f
dannisho <danielgelandu@gmail.com>
2022-10-14 23:55:24 -0700

    print

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main func
+ * Return: 0 on 
+ */
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(0);
+		c--;
+	}
+	putchar('\n');
+	return (0);
+}
