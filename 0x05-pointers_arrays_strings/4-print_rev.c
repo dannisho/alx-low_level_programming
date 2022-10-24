944e7c38c0425f54c6e936d7d414a72fbd115e47
saron kedir <saronkedir@gmail.com>
2022-10-24 08:15:16 -0400

    print reverse fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,17 +1,35 @@
 #include "main.h"
+
 /**
- * print_rev - Prints a string in reverse.
- * @s: string to be reserved.
+ * print_rev - prints a string in reverse.
+ *
+ * @s: store the string.
  */
 void print_rev(char *s)
 {
-	int len = 0, i;
+	int i;
 
-	while (s[i++])
-		len++;
-
-	for (i = len - 1; i >= 0; i--)
+	i = string_length(s) - 1;
+	while (i >= 0)
+	{
 		_putchar(s[i]);
-
+		i--;
+	}
 	_putchar('\n');
+}
+
+/**
+  * string_length - finds the length of a string.
+  * Return: length of c.
+  * @pointer: pointer.
+  */
+int string_length(char *pointer)
+{
+	int c = 0;
+
+	while (*(pointer + c) != '\0')
+	{
+		c++;
+	}
+	return (c);
 }
