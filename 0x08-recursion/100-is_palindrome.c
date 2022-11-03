707943a7de0ce2e44eed5669a175e62a17acc5ca
saron kedir <saronkedir@gmail.com>
2022-11-03 02:58:33 -0400

    advanced fix

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,17 +1,55 @@
 #include "main.h"
-
-int findLength(char *s);
-int helper(char *s, int len, int a);
-
 /**
- * is_palindrome - function with 1 argument
- * @s: char type pointer argument
+ * s_len - calculates the length of a string.
+ * @s: original string
  *
- * Description: test if string is palindrome
- * Return: 1 if palindrome or 0 if not
+ *
+ * Return: length of string.
+ */
+int s_len(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + s_len(s + 1));
+	}
+}
+/**
+ * pal - check if a string is a palindrome.
+ * @s: original string
+ * @i: initial point of recursion
+ * @len: length of string
+ * Return: 1 if is palindrome otherwise 0.
+ */
+int pal(char *s, int i, int len)
+{
+	if (*(s + i) == *(s + len - 1 - i) && i == (len / 2))
+	{
+		return (1);
+	}
+	else if (*(s + i) != *(s + len - 1 - i))
+	{
+		return (0);
+	}
+	else
+	{
+		return (pal(s, i + 1, len));
+	}
+}
+/**
+ * is_palindrome - check if the string is a palindrome ex ala.
+ * @s: original string
+ *
+ *
+ * Return: 1 if is alindrome otherwise 0.
  */
 int is_palindrome(char *s)
 {
-	return (helper(s, findLength(s) - 1, 0));
+	if (pal(s, 0, s_len(s)) == 1)
+		return (1);
+	else
+		return (0);
 }
-
